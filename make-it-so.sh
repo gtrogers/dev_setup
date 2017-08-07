a7cf9381ca0989b149eff3f4a9ad6eef13d005a7
Gareth Rogers <garethrogers@Gareths-MacBook-Pro.local>
2017-08-07 13:41:33 +1000

    Set password for dev user

--- make-it-so.sh
+++ make-it-so.sh
@@ -8,4 +8,4 @@ apt-add-repository ppa:ansible/ansible -y
 apt-get update
 apt-get install ansible -y
 
-ansible-playbook -i "localhost," -c local dev-setup.yml
+ansible-playbook -i "localhost," -c local dev-setup.yml --extra-vars="dev_user_password=`mkpasswd --method=sha-512 $PASS` github_user=$GITHUB"
